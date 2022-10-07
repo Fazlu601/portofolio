2b8c60fc2410637347e4f9d12e138bdd82c9f93a
Fazlu601 <fazrlu9575@gmail.com>
2022-10-08 01:18:05 +0700

    update-api

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,7 @@ const navButton = document.querySelectorAll('a.nav-links');
 
 
 async function getArticle() {
-    const response = await fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=66d0e8899e7740ce846aa4ba8ed7ea38');
+    const response = await fetch('http://api.mediastack.com/v1/news?access_key=81b9b357102884bb39214d5b35ffac00');
     if (!response.ok) {
         const message = `An error has occured: ${response.status}`;
         throw new Error(message);
@@ -20,24 +20,24 @@ async function getArticle() {
 }
 
     getArticle().then(data => {
-        data.articles.forEach((item)=> {
+        data.data.forEach((item)=> {
+            console.log(item);
            const parent = document.getElementById('article');
            let text = `<div class="card project">
-                            <img src="${item.urlToImage}" alt="">
+                            <img src="${item.image}" alt="">
                             <div class="card-description">
                                 <h3 class="card-title">${item.title}</h3>
                                 <p class="card-desc">Author: ${item.author}</p>
-                                <p class="card-desc">Update: ${item.publishedAt}</p>
+                                <p class="card-desc">Update: ${item.published_at}</p>
                                 <p class="card-desc">${item.description}</p>
                                 <a href="${item.url}" target="_blank" class="btn-outline w-100">View More <span class="fa fa-search"></span> </a>
                             </div>
                         </div>`
            parent.insertAdjacentHTML('afterbegin', text);
-        }).catch(error => {
-            error.message; // 'An error has occurred: 404'
-          });
-    });
+        });
+    }).catch(error => {
+        error.message; // 'An error has occurred: 404'
+      });       
 
 
-showArticle();
 
